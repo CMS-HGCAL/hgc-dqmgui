4ac73e6cc871d37d51aef65b46ad3993df8c767b
ykao <yu-wei.kao@cern.ch>
2026-05-17 13:53:23 +0200

    handle display of L44

--- hgcal-layouts.py
+++ hgcal-layouts.py
@@ -9,7 +9,7 @@ digis = "digis information"
 tb_modules = [
     'ML_F3WC_IH0197', 'ML_F3WC_IH0196', 'ML_F3WC_IH0198', 'ML_F3WC_IH0190',
     'ML_F3WC_IH0192', 'ML_F3WC_IH0191', 'ML_F3WC_IH0194', 'ML_F3WC_IH0182',
-    'ML_F3WC_IH0180', 'ML_F3WC_IH0199', 'TL-L44S1-TB2025'
+    'ML_F3WC_IH0180', 'ML_F3WC_IH0199', 'TL_L44S1_TB2025'
 ]
 
 # Layout configurations for DQM GUI with placeholders: {layer}, {module}
@@ -30,16 +30,16 @@ layout_configs = [
     # TrigPhase layouts
     #----------------------------------------------------------------------
     ("TrigPhase/Trigger Phase - ADC @ Layer {layer}",
-     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/(u8-v5) {module}/seedadcvstrigtime"),
+     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/({uv_coor}) {module}/seedadcvstrigtime"),
 
     ("TrigPhase/Trigger Phase - ToA @ Layer {layer}",
-     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/(u8-v5) {module}/seedtoavstrigtime"),
+     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/({uv_coor}) {module}/seedtoavstrigtime"),
 
     #----------------------------------------------------------------------
     # ADC layouts
     #----------------------------------------------------------------------
     ("ADC/Average ADC @ Layer {layer}",
-     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/(u8-v5) {module}/avgadc"),
+     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/({uv_coor}) {module}/avgadc"),
 
     #----------------------------------------------------------------------
     # TOT layouts
@@ -48,7 +48,7 @@ layout_configs = [
      "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/hex_avgtot_layer_{layer}"),
 
     ("TOT/TOT @ Layer {layer}",
-     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/(u8-v5) {module}/tot"),
+     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/({uv_coor}) {module}/tot"),
 
     #----------------------------------------------------------------------
     # TOA layouts
@@ -57,7 +57,7 @@ layout_configs = [
      "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/hex_avgtoa_layer_{layer}"),
 
     ("TOA/TOA @ Layer {layer}",
-     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/(u8-v5) {module}/toa"),
+     "HGCAL/EndCap_Minus/Layer_{layer}/Cassette_1/({uv_coor}) {module}/toa"),
 
     #----------------------------------------------------------------------
     # RecHits layouts
@@ -81,11 +81,14 @@ layout_configs = [
 #           [{ 'path': "HGCAL/EndCap_Minus/Layer_1/Cassette_1/hex_avgadc_layer_1", 'description': quality + hgcallink }])
 #------------------------------------------------------------------------------------------------------------------------
 
+uv_coordinates = ["u8-v5"]*10 + ["u0-v0"]
+
 for i, layer in enumerate([1,2,3,4,5,6,7,8,9,10,44]):
 
     module = tb_modules[i]
+    uv_coor = uv_coordinates[i]
 
     for title_template, path_template in layout_configs:
-        title = title_template.format(layer=layer, module=module)
-        path = path_template.format(layer=layer, module=module)
+        title = title_template.format(layer=layer, module=module, uv_coor=uv_coor)
+        path = path_template.format(layer=layer, module=module, uv_coor=uv_coor)
         hgcallayout(dqmitems, title, [{'path': path, 'description': quality + hgcallink}])
